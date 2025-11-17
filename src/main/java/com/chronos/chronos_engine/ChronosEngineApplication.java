44bfc47c5194254dfa85cbc60c78c0d812d5359b
AEin-yx <aeinha.sha@gmail.com>
2025-11-17 16:42:32 +0530

    init: chronos-engine project setup

---
+++ src/main/java/com/chronos/chronos_engine/ChronosEngineApplication.java
@@ -0,0 +1,13 @@
+package com.chronos.chronos_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChronosEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChronosEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chronos/chronos_engine/ChronosEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chronos.chronos_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChronosEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
